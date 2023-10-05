5fc8ab307c5660feccc311aaf72fa96a4f523183
ext_larh <larsho@netcompany.com>
2023-10-05 17:52:25 +0200

    Admin and confirmation pages

--- src/RoutePlanning.Client.Web/Pages/Admin.razor.cs
+++ src/RoutePlanning.Client.Web/Pages/Admin.razor.cs
@@ -3,7 +3,7 @@
 namespace RoutePlanning.Client.Web.Pages
 {
     
-    public partial class SearchResults
+    public partial class Admin
     {
         [Inject]
         public NavigationManager? NavManager { get; set; }
@@ -41,7 +41,8 @@ namespace RoutePlanning.Client.Web.Pages
                         new Tags { tag = "Cheapest" },
                         new Tags { tag = "Telstar exclusive" },
                     },
-                    Id = 1
+                    Id = 1,
+                    Weight = 10
                     
 
                 },
@@ -62,7 +63,8 @@ namespace RoutePlanning.Client.Web.Pages
                     {
                         new Tags { tag = "Fastest" }
                     },
-                    Id = 2
+                    Id = 2,
+                    Weight = 20
                 }
             };
         }
@@ -75,6 +77,8 @@ namespace RoutePlanning.Client.Web.Pages
             public decimal Price { get; set; }
             public DateTime Date { get; set; }
             public List<Tags>? Tags { get; set; }
+
+            public double? Weight { get; set; }
             
             public int Id { get; set; }
         }
@@ -99,6 +103,26 @@ namespace RoutePlanning.Client.Web.Pages
             };
         }
         
+        /*private async Task CancelShipping(int routeId)
+        {
+            // TODO: Send a request to the API to cancel the shipping with the provided routeId.
+    
+            // This is a placeholder for the actual API call.
+            // You'll replace the below lines with your API call when your backend is ready.
+            // Example:
+            // var response = await HttpClient.DeleteAsync($"https://api.example.com/cancel-booking/{routeId}");
+            // if(response.IsSuccessStatusCode) 
+            // {
+            //     // Handle successful cancellation: e.g., show a confirmation message, refresh the list, etc.
+            // }
+            // else 
+            // {
+            //     // Handle errors: e.g., show an error message.
+            // }
+
+            Console.WriteLine($"Requested to cancel booking with ID: {routeId}");
+        }*/
+
         
     }
     
