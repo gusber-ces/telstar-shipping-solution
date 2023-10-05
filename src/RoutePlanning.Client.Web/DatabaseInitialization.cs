9b77ca4754c5ee98184c48461cd9d464e16e9b99
gusber-ces <gustav.bern@drift.oslo.kommune.no>
2023-10-05 15:13:34 +0200

    hardcode routes

--- src/RoutePlanning.Client.Web/DatabaseInitialization.cs
+++ src/RoutePlanning.Client.Web/DatabaseInitialization.cs
@@ -38,10 +38,76 @@ public static class DatabaseInitialization
         var warsaw = new Location("Warsaw");
         await context.AddAsync(warsaw);
 
+        var kapstaden = new Location("Kapstaden");
+        var hvalbugten = new Location("Hvalbugten");
+        var dragebjerget = new Location("Dragebjerget");
+        var luanda = new Location("Luanda");
+        var victoriafaldene = new Location("Victoriafaldene");
+        var mozambique = new Location("Mozambique");
+        var kabalo = new Location("Kabalo");
+        var congo = new Location("Congo");
+        var slavekysten = new Location("Slavekysten");
+        var darfur = new Location("Darfur");
+        var bahrelGhazal = new Location("Bahrel Ghazal");
+        var victoriasoeen = new Location("Victoriasoeen");
+        var zanzibar = new Location("Zanzibar");
+        var kapGuardafui = new Location("Kap Guardafui");
+        var suakin = new Location("Suakin");
+        var omdurman = new Location("Omdurman");
+        var wadai = new Location("Wadai");
+        var tripoli = new Location("Tripoli");
+        var tunis = new Location("Tunis");
+        var marrakesh = new Location("Marrakesh");
+        var dakar = new Location("Dakar");
+        var sierraLeone = new Location("Sierra Leone");
+        var timbuktu = new Location("Timbuktu");
+        var sahara = new Location("Sahara");
+        var addisAbeba = new Location("Addis Abeba");
+        var guldkysten = new Location("Guldkysten");
+        var tanger = new Location("Tanger");
+        var cairo = new Location("Cairo");
+
+        
+        
         CreateTwoWayConnection(berlin, warsaw, 573);
         CreateTwoWayConnection(berlin, copenhagen, 763);
         CreateTwoWayConnection(berlin, paris, 1054);
         CreateTwoWayConnection(copenhagen, paris, 1362);
+        CreateTwoWayConnection(kapstaden, hvalbugten, 4);
+        CreateTwoWayConnection(hvalbugten, victoriafaldene, 4);
+        CreateTwoWayConnection(victoriafaldene, dragebjerget, 3);
+        CreateTwoWayConnection(victoriafaldene, mozambique, 5);
+        CreateTwoWayConnection(dragebjerget, mozambique, 4);
+        CreateTwoWayConnection(mozambique, luanda, 10);
+        CreateTwoWayConnection(mozambique, zanzibar, 3);
+        CreateTwoWayConnection(mozambique, victoriasoeen, 6);
+        CreateTwoWayConnection(zanzibar, kapGuardafui, 6);
+        CreateTwoWayConnection(zanzibar, victoriasoeen, 5);
+        CreateTwoWayConnection(luanda, congo, 3);
+        CreateTwoWayConnection(luanda, kabalo, 4);
+        CreateTwoWayConnection(congo, slavekysten, 5);
+        CreateTwoWayConnection(congo, wadai, 6);
+        CreateTwoWayConnection(congo, darfur, 6);
+        CreateTwoWayConnection(slavekysten, timbuktu, 5);
+        CreateTwoWayConnection(slavekysten, wadai, 7);
+        CreateTwoWayConnection(slavekysten, darfur, 7);
+        CreateTwoWayConnection(timbuktu, guldkysten, 4);
+        CreateTwoWayConnection(timbuktu, sierraLeone, 5);
+        CreateTwoWayConnection(guldkysten, sierraLeone, 5);
+        CreateTwoWayConnection(sierraLeone, dakar, 4);
+        CreateTwoWayConnection(dakar, marrakesh, 8);
+        CreateTwoWayConnection(marrakesh, tanger, 2);
+        CreateTwoWayConnection(tanger, tunis, 5);
+        CreateTwoWayConnection(tanger, sahara, 5);
+        CreateTwoWayConnection(tunis, tripoli, 3);
+        CreateTwoWayConnection(tripoli, omdurman, 6);
+        CreateTwoWayConnection(omdurman, cairo, 4);
+        CreateTwoWayConnection(wadai, darfur, 4);
+        CreateTwoWayConnection(sahara, darfur, 8);
+        CreateTwoWayConnection(omdurman, darfur, 3);
+        CreateTwoWayConnection(darfur, suakin, 4);
+        CreateTwoWayConnection(suakin, addisAbeba, 3);
+        CreateTwoWayConnection(addisAbeba, kapGuardafui, 3);
     }
 
     private static async Task SeedUsers(RoutePlanningDatabaseContext context)
