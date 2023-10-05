89cad77a2348954bab5092dc09ac252d8b4b2b56
ext_larh <larsho@netcompany.com>
2023-10-05 21:46:38 +0200

    Routing from search

--- src/RoutePlanning.Client.Web/Pages/Search.razor.cs
+++ src/RoutePlanning.Client.Web/Pages/Search.razor.cs
@@ -1,17 +1,29 @@
 ﻿using System.Runtime.Intrinsics.Arm;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using RoutePlanning.Domain;
 using RoutePlanning.Domain.Locations.Services;
 
+
 namespace RoutePlanning.Client.Web.Pages
 {
+    
     public sealed partial class Search
     {
+        
+        
+        [Inject]
+        public NavigationManager? NavManager { get; set; }
+       
+        
+        
         private string Origin { get; set; } = "CountryA";
         private string Destination { get; set; } = "CountryA";
         
         private List<string> countries = new() { "CountryA", "CountryB", "CountryC" };
         
+        
+
         private void OnOriginChanged(string value)
         {
             Origin = value;
@@ -48,8 +60,9 @@ namespace RoutePlanning.Client.Web.Pages
                 SelectedCategories.Remove(category);
             }
         }
-
-
+        
+    
+        
         private void SubmitForm()
         {
             // Here, handle form submission, e.g., send the form data to an API, save it in a database, etc.
@@ -76,6 +89,10 @@ namespace RoutePlanning.Client.Web.Pages
             // For demonstration purposes, I'll just print the array to the console (useful for debugging)
 
             Console.WriteLine(string.Join(", ", FormData));
+            if (NavManager != null)
+            {
+                NavManager.NavigateTo($"/searchresults");
+            }
         }
     }
 }
